a1a812f39ebc6ba2540edb3d92a61de83b4d254c
Sukhpreet Singh <peadevp@gmail.com>
2023-09-21 00:48:05 +0530

    Added Google Auth Example

---
+++ sveltekit/google-oauth/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Profile</h1>
+<p>User id: {data.userId}</p>
+<p>Name: {data.name}</p>
+<img src={data.image} alt={` ${data.name} Avatar Image `} />
+<form method="post" action="?/logout" use:enhance>
+	<input type="submit" value="Sign out" />
+</form>
---
+++ sveltekit/google-oauth/src/routes/login/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Sign in</h1>
+<a href="/login/google">Sign in with Google</a>
