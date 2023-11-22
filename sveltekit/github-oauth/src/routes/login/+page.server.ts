ce1dab01b4237bf06a7376d58bdf9e24976b1e15
Humberto Rodríguez A. <rhumbertgz@users.noreply.github.com>
2023-11-22 15:21:39 +0000

    Add explicit types to avoid 'any' warnings

--- sveltekit/github-oauth/src/routes/login/+page.server.ts
+++ sveltekit/github-oauth/src/routes/login/+page.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (session) throw redirect(302, '/');
 	return {};
