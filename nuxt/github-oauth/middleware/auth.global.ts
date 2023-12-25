c0dc4b4d8668e27cf21bac129dba77e06eb35ee9
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-12-25 22:53:50 +0200

    chore: fix typo

--- nuxt/github-oauth/middleware/auth.global.ts
+++ nuxt/github-oauth/middleware/auth.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware(async () => {
 	// It has been in nuxt for a while but not documented ?
 	// You could use bare `$fetch`, but the main issue is that it would not use
 	// client headers for local request (nitro side), so the middleware
-	// wont see session cookie eventhou it was there, but not sent.
+	// wont see session cookie even though it was there, but not sent.
 	// `useRequestFetch` fixes it by using normal `$fetch` on client side
 	// and nitro's own `$fetch` will will inherit headers from current event
 	// and so cookies as well
