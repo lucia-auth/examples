827ff5bce18e4d256ca7eb57a229aececaafc0f6
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-12-25 22:14:31 +0200

    chore: apply nuxt best practices

--- nuxt/github-oauth/middleware/auth.global.ts
+++ nuxt/github-oauth/middleware/auth.global.ts
@@ -1,6 +1,16 @@
 export default defineNuxtRouteMiddleware(async () => {
 	const user = useUser();
-	const { data, error } = await useFetch("/api/user");
-	if (error.value) throw createError("Failed to fetch data");
-	user.value = data.value?.user ?? null;
+	// It has been in nuxt for a while but not documented ?
+	// You could use bare `$fetch`, but the main issue is that it would not use
+	// client headers for local request (nitro side), so the middleware
+	// wont see session cookie eventhou it was there, but not sent.
+	// `useRequestFetch` fixes it by using normal `$fetch` on client side
+	// and nitro's own `$fetch` will will inherit headers from current event
+	// and so cookies as well
+	const fetch = useRequestFetch();
+	const data = await fetch('/api/user')
+		.catch(() => {
+			throw createError("Failed to fetch data")
+		})
+	user.value = data.user ?? null;
 });
