9a657ceec70c46db9ef5a1ba6d68eb74d446bb22
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-01-21 15:08:12 +0900

    fix form action

--- sveltekit/username-and-password/src/routes/login/+page.svelte
+++ sveltekit/username-and-password/src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <h1>Sign in</h1>
-<form method="post" use:enhance action="/signup">
+<form method="post" use:enhance>
 	<label for="username">Username</label>
 	<input name="username" id="username" /><br />
 	<label for="password">Password</label>
